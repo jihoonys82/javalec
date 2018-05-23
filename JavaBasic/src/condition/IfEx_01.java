c79018b96f8255ed16664a42a11a5ce30a002e5c
jihoonys82 <jihoonys82@gmail.com>
2018-05-23 19:32:46 +0900

    condition chapter added

---
+++ JavaBasic/src/condition/IfEx_01.java
@@ -0,0 +1,24 @@
+package condition;
+
+public class IfEx_01 {
+	public static void main(String[] args) {
+		
+		// if statement 
+	
+		// if(condition) 
+		// executed when the condition is true(boolean).
+		
+		if(true) 
+			System.out.println("Single line");
+		//single line statement. it is NOT recommended.
+		
+		
+		if(false) {
+			System.out.println("test");
+			System.out.println("is it printed?");
+		}
+		
+		System.out.println("it is not belonging if statement.");
+		
+	}
+}
